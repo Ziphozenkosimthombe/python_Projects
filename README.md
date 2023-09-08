68e002afcc3519f90afd9223f424b2a6655f3e9e
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-09-09 00:36:23 +0200

    password-generator

---
+++ README.md
@@ -0,0 +1,6 @@
+password generator:
+1. you will choose how many letter you want in your passsword and it will random generate that letters.
+2. choose how many symbols do you want and random generate that symbol.
+3. chooose how may number and random generate that numbers.
+
+at the end going to print the password of choice add all the lists.
---
+++ password_generator.py
@@ -0,0 +1,38 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+           'n', 'o', 'p', 'q', 'r', 's', 'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+           'N', 'O', 'P', 'Q', 'R', 'S', 'W', 'X', 'Y', 'Z']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+signs = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', '{', '}',
+         '[', ']', '|', ':', '~', '`']
+
+print('welcome to the generative password!!!!!!!')
+
+user_letter = int(input('How many letter would like your password have?:  '))
+user_signs = int(input('how many signs would you like your password have?:  '))
+user_number = int(input('how many numbers would you like password have ?:  '))
+
+
+password_letter = []
+for i in range(1, user_letter+1):
+    char = random.choice(letters)
+    password_letter += char
+
+for i in range(1, user_signs+1):
+    char = random.choice(signs)
+    password_letter += char
+
+for i in range(1, user_number):
+    char = random.choice(numbers)
+    password_letter += char
+
+print(password_letter)
+
+random.shuffle(password_letter)
+print(password_letter)
+password = ''
+for char in password_letter:
+    password += char
+print(password)
